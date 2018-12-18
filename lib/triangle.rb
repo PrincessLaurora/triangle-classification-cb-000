5e326d3ba1467ecd1a456255ec6c25f2c61635d1
Laura Felce <crepu83@yahoo.it>
2018-12-18 15:53:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,9 +19,9 @@ class Triangle
   end
 
   class TriangleError < StandardError
-    if a = 0 && b = 0 && c = 0
-      raise TriangleError
+    def message
+      puts "Illegal"
     end
-  end
+
 
 end
